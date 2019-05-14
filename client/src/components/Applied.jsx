5108f053d1ad02d20a1f5e9542d8636d6a0a7cc0
Bernard <laubernardsy@gmail.com>
2019-05-14 19:01:08 -0400

    [Added] Modal for Applied jobs

--- client/src/components/Applied.jsx
+++ client/src/components/Applied.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import Modal from './Modal.jsx';
+
+const modalStyle = {
+	overlay: {
+		backgroundColor: "rgba(0, 0, 0, 0.5)"
+	}
+};
+
+const mainStyle = {
+	app: {
+		margin: "120px 0"
+	},
+	button: {
+		width: 150,
+		padding: "12px 20px",
+		color: "#fff",
+		border: 0,
+		margin: "0 auto",
+		display: "block",
+		backgroundColor: "#37d6c1",
+		borderRadius: 3
+	}
+};
 
 class Applied extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      applied: []
+      applied: [],
+      isModalOpen: false,
+      isInnerModalOpen: false
     }
     this.getAppliedJobs = this.getAppliedJobs.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+		this.openModal = this.openModal.bind(this);
   }
   getAppliedJobs() {
     var state = this;
@@ -20,6 +47,16 @@ class Applied extends React.Component {
       } 
     });
   }
+  closeModal() {
+		this.setState({
+			isModalOpen: false
+		});
+	}
+	openModal() {
+		this.setState({
+			isModalOpen: true
+		});
+	}
   componentDidMount() {
     this.getAppliedJobs();
   }
@@ -42,7 +79,7 @@ class Applied extends React.Component {
           </thead>
             <tbody>
               {this.state.applied.map((job) => (
-                <tr key={job.job_id}>
+                <tr key={job.job_id} onClick={this.openModal}>
                   <td><a href={job.href.replace(/[$][0-9]/g, "?")} target="_blank">{job.title}</a></td>
                   <td>{job.subtitle}</td>
                   <td>{job.date_applied.substring(0, job.date_applied.length-5).replace(/[T]/," ")}</td>
@@ -52,6 +89,22 @@ class Applied extends React.Component {
               ))}
             </tbody>
         </table>
+        <Modal
+					isModalOpen={this.state.isModalOpen}
+					closeModal={this.closeModal}
+					style={modalStyle}
+				>
+          
+          <button
+						style={{
+							...mainStyle.button,
+							margin: 0,
+							width: "auto",
+							marginTop: 10
+						}}
+						onClick={this.closeModal}
+					>Close</button>
+        </Modal>
       </div>
     )
   }
